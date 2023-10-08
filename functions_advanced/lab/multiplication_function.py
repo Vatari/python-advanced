f0759e357b1d2c16d28a9d4d100870f92bc8ef78
vatari <majestic13@gmail.com>
2023-10-08 09:58:36 +0300

    lab 2

---
+++ functions_advanced/lab/multiplication_function.py
@@ -0,0 +1,12 @@
+from functools import reduce
+
+
+def multiply(*args):
+    # result = 1
+    # for el in args:
+    #     result *= el
+    # return result
+    return reduce(lambda x, y: x * y, args)
+
+
+print(multiply(1, 4, 5))
